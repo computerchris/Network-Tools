8cc08738188087058598e3dc62335dd3c1474f13
computerchris <computerchris9001@gmail.com>
2015-03-14 15:17:26 -0400

    Update find.php

--- find.php
+++ find.php
@@ -1,6 +1,6 @@
 <?php
 $ip4me = strip_tags($_POST['geoiphtmlvalue']);
-$xml = file_get_contents('http://api.ipinfodb.com/v3/ip-city/?key=fee3fc0a7f640060078fe12d35a44f2003fbcea9da1bcd208d55ee66dc761b29&ip=' . $ip4me . '&format=xml');
+$xml = file_get_contents('http://api.ipinfodb.com/v3/ip-city/?key=X&ip=' . $ip4me . '&format=xml');
 echo $xml;
 
 ?>
